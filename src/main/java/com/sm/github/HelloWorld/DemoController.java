6e430b23fa9a382eef38828c13845cc6f4cdf7e0
Shilpi Mehta <shilpi.mehta@telusinternational.com>
2025-02-27 16:16:44 +0530

    added controller

---
+++ src/main/java/com/sm/github/HelloWorld/DemoController.java
@@ -0,0 +1,15 @@
+package com.sm.crud.crud.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class DemoController {
+
+	@GetMapping("/hello")
+	public String getMessage(){
+		return "Hello ! Welcome to Github Action Demo Project";
+	}
+}
